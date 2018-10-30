9fd25847777db8ce1e7a5bbe145f4c56e1c54d8d
glmp <44590183+glmoraes@users.noreply.github.com>
2018-10-30 18:20:25 -0300

    Primeira versão

---
+++ hello-git/src/br/com/glmoraes/git/HelloGit.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.com.glmoraes.git;
+
+/**
+ * @author Gina
+ * Exemplo de integragração com o GitHub
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git!!");
+
+	}
+
+}
